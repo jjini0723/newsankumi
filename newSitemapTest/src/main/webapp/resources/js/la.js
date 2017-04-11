a7afb24b4b01438edada7fac61c44f30fa0ecade
Crystaldrum <crystalthoon@gmail.com>
2017-04-11 13:15:02 +0900

    by LTH
    뒷단 파일 추가  by LTH

---
+++ newSitemapTest/src/main/webapp/resources/js/la.js
@@ -0,0 +1,278 @@
+/**
+ * 
+ */
+
+function dfa(carArray, walkArray, tradiArray){
+	/*var car1 = {
+			y : '37.4833729',
+			x : '126.9797921'
+	};
+	var walk1 = {
+			y : '37.4993211',
+			x : '127.0527393'
+	};
+	var tradi1 = {
+			y : '37.5118467',
+			x : '127.0863366'
+	};
+	var car = new Array();
+	var walk = new Array();
+	var tradi = new Array();
+	car.push(car1);
+	walk.push(walk1);
+	tradi.push(tradi1);*/
+////////////////////////////////수동으로 자동차1,도보1,대중교통1 선택 가정해서 좌표 객체화 해서 배열에 집어넣음
+	
+	
+	
+	
+	if(carArray.length!=0){//자동차가 선택되지 않으면 배열 길이가 0이라 이 ajax는 작동하지않음
+		var avg = 0;
+		var count =0;
+		$.each(carArray,function(index,item){ //자동차가 선택 된 회수만큼 작동 희망목적지가 3곳 다 자동차면 3번 돈다
+			$.each(perfect,function(index,item1){ // 선택되어있는 아파트만큼 돈다
+				$.ajax({
+					type : "POST",	
+					 async: false,
+					 url : "https://apis.skplanetx.com/tmap/routes?version=1&format=json&reqCoordType=WGS84GEO&resCoordType=WGS84GEO&startX="+item1.x+"&startY="+item1.y+"&startName=a&endName=b&endX="+item.x+"&endY="+item.y+"&appKey=5466b076-3f62-3faf-ac9d-c3002f0fec2d",
+					 success : function(data){
+						 yebi = data.features[0].properties.totalTime//시간만 빼온다
+						 if(item1.car1==''){//소요 시간을 1번부터 돌면서 빈곳에 집어넣음, 만약 자동차로 3개가 골라졌으면 1,2,3번 다 찬다
+							 item1.car1=yebi;
+							 avg=avg+yebi;
+							 ++count;
+							 if(count==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.car1avg=avg/count;
+									 item.car1point=(item.car1avg/item.car1).toFixed(5);
+								 })
+							 }
+				 		 }else if(item1.car2==''){
+				 			 item1.car2=yebi;
+						 	 avg=avg+yebi;
+							 ++count;
+							 if(count==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.car2avg=avg/count;
+									 item.car2point=(item.car2avg/item.car2).toFixed(5);
+								 })
+							 }
+						 }else if(item1.car3==''){
+							 item1.car3=yebi;
+						 	 avg=avg+yebi;
+							 ++count;
+							 if(count==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.car3avg=avg/count;
+									 item.car3point=(item.car3avg/item.car3).toFixed(5);
+								 })
+							 }
+						 }else if(item1.car4==''){
+							 item1.car4=yebi;
+						 	 avg=avg+yebi;
+							 ++count;
+							 if(count==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.car4avg=avg/count;
+									 item.car4point=(item.car4avg/item.car4).toFixed(5);
+								 })
+							 }
+						 }else if(item1.car5==''){
+							 item1.car5=yebi;
+						 	 avg=avg+yebi;
+							 ++count;
+							 if(count==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.car5avg=avg/count;
+									 item.car5point=(item.car5avg/item.car5).toFixed(5);
+								 })
+							 }
+						 }
+					 },
+					 error : function(e){
+						 
+					 }
+				})
+			})
+		})
+	}
+	if(walkArray.length!=0){//도보가 선택되지 않았으면 작동 안함
+		var avg1 = 0;
+		var count1 =0;
+		$.each(walkArray,function(index,item){//도보로 설정 된 만큼 돈다
+			$.each(perfect,function(index,item1){//아파트 개수만큼 돈다 ex)도보로 2개 설정 된상태에서 아파트가 200개 면 url400번 날린다
+				$.ajax({
+					type : "POST",		
+					 async: false,
+					 url : "https://apis.skplanetx.com/tmap/routes/pedestrian?version=1&format=json&reqCoordType=WGS84GEO&resCoordType=WGS84GEO&startX="+item.x+"&startY="+item.y+"&startName=a&endName=b&endX="+item1.x+"&endY="+item1.y+"&appKey=5466b076-3f62-3faf-ac9d-c3002f0fec2d",
+					 success : function(data){
+						 yebi = data.features[0].properties.totalTime//총 시간만 출력
+						 if(item1.walk1==''){//빈곳 찾아 가기
+							 item1.walk1=yebi;
+							 avg1=avg1+yebi;
+							 ++count1;
+							 if(count1==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.walk1avg=avg1/count1;
+									 item.walk1point=(item.walk1avg/item.walk1).toFixed(5);
+								 })
+							 }
+						 }else if(item1.walk2==''){
+							 item1.walk2=yebi;
+						 	 avg1=avg1+yebi;
+							 ++count1;
+							 if(count1==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.walk2avg=avg1/count1;
+									 item.walk2point=(item.walk2avg/item.walk2).toFixed(5);
+								 })
+							 }
+						 }else if(item1.walk3==''){
+							 item1.walk3=yebi;
+						 	 avg1=avg1+yebi;
+							 ++count1;
+							 if(count1==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.walk3avg=avg1/count1;
+									 item.walk3point=(item.walk3avg/item.walk3).toFixed(5);
+								 })
+							 }
+						 }else if(item1.walk4==''){
+							 item1.walk4=yebi;
+						 	 avg1=avg1+yebi;
+							 ++count1;
+							 if(count1==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.walk4avg=avg1/count1;
+									 item.walk4point=(item.walk4avg/item.walk4).toFixed(5);
+								 })
+							 }
+						 }else if(item1.walk5==''){
+							 item1.walk5=yebi;
+						 	 avg1=avg1+yebi;
+							 ++count1;
+							 if(count1==perfect.length){
+								 $.each(perfect,function(index,item){
+									 item.walk5avg=avg1/count1;
+									 item.walk5point=(item.walk5avg/item.walk5).toFixed(5);
+								 })
+							 }
+						 }
+					 },
+					 error : function(e){
+						 
+					 }
+				})
+			})
+		})
+	}
+	if(tradiArray.length!=0){//대중교통이 선택되지 않았으면 작동 안함
+		var avg2 = 0;
+		var count2 =0;
+		$.each(tradiArray,function(index,item){//대중교통이 선택 된 횟수 만큼
+			$.each(perfect,function(index,item1){//아파트 개수 만큼
+				jQuery.ajaxSettings.traditional = true;
+				$.ajax({
+					type:"post",
+					async: false,
+					contentType : "application/json;charset=utf-8",
+					dataType : "json",
+					url:"https://maps.googleapis.com/maps/api/directions/json?origin="+item1.y+","+item1.x+"&destination="+item.y+","+item.x+"&mode=transit&key=AIzaSyCY7dyy4vD8x7q9Is03binfIE9TgEoVA5w",
+					success:function(data){
+						yebi = data.routes[0].legs[0].duration.value;//소요시간만 뺀다
+						if(item1.tradi1==''){// 자리에 찾아 들어간다
+							item1.tradi1=yebi;
+							avg2=avg2+yebi;
+							++count2;
+							if(count2==perfect.length){
+								$.each(perfect,function(index,item){
+									item.tradi1avg=avg2/count2;
+									item.tradi1point=(item.tradi1avg/item.tradi1).toFixed(5);
+								})
+							}
+						}else if(item1.tradi2==''){
+							item1.tradi2=yebi;
+						 	avg2=avg2+yebi;
+							++count2;
+							if(count2==perfect.length){
+								$.each(perfect,function(index,item){
+									item.tradi2avg=avg2/count2;
+									item.tradi2point=(item.tradi2avg/item.tradi2).toFixed(5);
+								})
+							}
+						}else if(item1.tradi3==''){
+							item1.tradi3=yebi;
+						 	avg2=avg2+yebi;
+							++count2;
+							if(count2==perfect.length){
+								$.each(perfect,function(index,item){
+									item.tradi3avg=avg2/count2;
+									item.tradi3point=(item.tradi3avg/item.tradi3).toFixed(5);
+								})
+							}
+						}else if(item1.tradi4==''){
+							item1.tradi3=yebi;
+						 	avg2=avg2+yebi;
+							++count2;
+							if(count2==perfect.length){
+								$.each(perfect,function(index,item){
+									item.tradi3avg=avg2/count2;
+									item.tradi3point=(item.tradi3avg/item.tradi3).toFixed(5);
+								})
+							}
+						}else if(item1.tradi5==''){
+							item1.tradi3=yebi;
+						 	avg2=avg2+yebi;
+							++count2;
+							if(count2==perfect.length){
+								$.each(perfect,function(index,item){
+									item.tradi3avg=avg2/count2;
+									item.tradi3point=(item.tradi3avg/item.tradi3).toFixed(5);
+								})
+							}
+						}
+					},
+					error:function(e){
+						console.log(e)
+					}
+				})
+			})
+		})
+	}
+	
+	$.each(perfect,function(index,item){
+			item.totalpoint = parseFloat(item.car1point)+parseFloat(item.car2point)+parseFloat(item.car3point)+parseFloat(item.walk1point)+parseFloat(item.walk2point)+parseFloat(item.walk3point)+parseFloat(item.tradi1point)+parseFloat(item.tradi2point)+parseFloat(item.tradi3point);
+
+	})
+	var perfect2 = new Array();
+	var perfect3 = new Array();
+	var save=0;
+
+//	$.each(perfect,function(index,item){
+//		perfect2.push(item.totalpoint);
+//	})
+	
+	
+	
+	perfect.sort(function(a,b){
+		if(a.totalpoint>b.totalpoint){
+			return -1;
+		}
+		if(a.totalpoint<b.totalpoint){
+			return 1;
+		}
+		return 0;
+	})
+	
+	console.log(perfect);
+	
+	
+//	for(var i=perfect2.length;i=0;i--){
+//		for(var j=0;j<perfect.length;j++){
+//			if(perfect2[i]==perfect[j].totalpoint){
+//				perfect3.push(perfect[j]);
+//			}
+//		}
+//	}
+	console.log(perfect3);
+}
---
+++ newSitemapTest/src/main/webapp/resources/js/mapShowResult.js
@@ -0,0 +1,190 @@
+/**
+ * 
+ */
+
+function call(emdName){
+	
+	// 지도 타입 변경 컨트롤을 생성한다
+    var mapTypeControl = new daum.maps.MapTypeControl();
+
+    // 지도의 상단 우측에 지도 타입 변경 컨트롤을 추가한다
+    map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);   
+
+    // 지도에 확대 축소 컨트롤을 생성한다
+    var zoomControl = new daum.maps.ZoomControl();
+
+    // 지도의 우측에 확대 축소 컨트롤을 추가한다
+    map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+    
+ 	// 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new daum.maps.services.Geocoder();
+
+    // 지도 클릭 이벤트를 등록한다 (좌클릭 : click, 우클릭 : rightclick, 더블클릭 : dblclick)
+    daum.maps.event.addListener(map, 'click', function (mouseEvent) {
+       console.log('지도에서 클릭한 위치의 좌표는 ' + mouseEvent.latLng.toString() + ' 입니다.');
+    });   
+    // 지도 드래깅 이벤트를 등록한다 (드래그 시작 : dragstart, 드래그 종료 : dragend)
+    daum.maps.event.addListener(map, 'drag', function () {
+       var message = '지도를 드래그 하고 있습니다. ' + 
+                   '지도의 중심 좌표는 ' + map.getCenter().toString() +' 입니다.';
+       console.log(message);
+    });
+	var testArray = [];
+	//var emdName = '삼성동';
+	var emdNameArray = new Array();
+   	//var emdName = new Object();
+   	//emdName.emdName1 = '삼성동';
+   	//emdName.emdName1 = '상계동';
+   	//emdName.emdName1 = '부평동';
+   	//emdNameArray.push(emdName);
+  	testArray.push('11680105');
+  	testArray.push('11740102');
+   	testArray.push('28237101');
+   	
+    	
+    for(var i in testArray){
+    	$.ajax({
+    		url : "http://apis.vworld.kr/2ddata/ademd/data?apiKey=CCA36BB7-0DA8-3EE7-8836-D4814D529510&domain=http://localhost:8888&emdCd="+testArray[i]+"&srsName=EPSG:4326&output=json",
+    		dataType : "jsonp",
+    		jsonp : "callback",
+    		success : function(rtndata) {
+    			console.log(rtndata.featureCollection.features[0].geometry.coordinates);
+    			var list = rtndata.featureCollection.features[0].geometry.coordinates;
+    			var newList = new Array();
+    		 	var newListX = new Array(); 
+    		 	var polygonPath = [];
+    			newList = list[0];
+    			for(var i in newList){
+    				newList[i].reverse();
+    			}
+    			alert(newList);
+    		/* list =rtndata.featureCollection.features[0].geometry.coordinates; */	
+    		/* 	alert(newList); */
+    		
+    		
+    			// 다각형을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 다각형을 표시합니다
+    			for(var i in newList){
+    				polygonPath.push(new daum.maps.LatLng(newList[i][0],newList[i][1]));
+    			}
+    			// 지도에 표시할 다각형을 생성합니다
+	    		var polygon = new daum.maps.Polygon({
+    		        path:polygonPath, // 그려질 다각형의 좌표 배열입니다
+    		        strokeWeight: 3, // 선의 두께입니다
+    		        strokeColor: '#39DE2A', // 선의 색깔입니다
+    		        strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    		        strokeStyle: 'longdash', // 선의 스타일입니다
+    		        fillColor: 'gray', // 채우기 색깔입니다
+    		        fillOpacity: 0.7 // 채우기 불투명도 입니다
+    		    });
+    		    // 지도에 다각형을 표시합니다
+    		    polygon.setMap(map);
+    		},
+    		error : function(e){
+    			console.log(e);
+    		} 
+    	});
+    }
+    jQuery.ajaxSettings.traditional = true;
+	$.ajax({
+		type : "POST",
+		url : "getDoroCD",
+		contentType : "application/json; charset=utf-8",
+		data : emdName,
+		success : function(data) {
+			//주소로 좌표를 검색합니다
+			var kaptAddr = new Array();
+ 			var kaptName = new Array();
+			var coordsList = [];
+			for(var i in data) {
+				kaptAddr.push(data[i].kaptAddr);
+				kaptName.push(data[i].kaptName);
+				//alert('주소 : '+kaptAddr[i]+' 아파트이름 : '+kaptName[i]);
+			}
+			$.each(kaptAddr,function(index,item) {
+				geocoder.addr2coord(item, function(status, result) {
+    				if (status === daum.maps.services.Status.OK) {
+    			       var coords = new daum.maps.LatLng(result.addr[0].lat, result.addr[0].lng);
+    			       var marker = new daum.maps.Marker({
+    			           map: map,
+    			           position: coords
+    			       });
+    			       var perfect2={
+    			    		    y : result.addr[0].lat,//y좌표
+	    						x : result.addr[0].lng,//x좌표
+	    						kaptAddr : kaptAddr[index],//아파트주소
+	    						kaptName : kaptName[index],//아파트이름
+	    						car1 : '',//자동차로 3곳을 설정할 경우를 대비해서 아예 1,2,3번만듬
+	    						car2 : '',
+	    						car3 : '',
+	    						car4 : '',
+	    						car5 : '',
+	    						car1avg : '',
+	    						car2avg : '',
+	    						car3avg : '',
+	    						car4avg : '',
+	    						car5avg : '',
+	    						car1point : 0,
+	    						car2point : 0,
+	    						car3point : 0,
+	    						car4point : 0,
+	    						car5point : 0,
+	    						walk1 : '',//도보 동일
+	    						walk2 : '',
+	    						walk3 : '',
+	    						walk4 : '',
+	    						walk5 : '',
+	    						walk1avg :'',
+	    						walk2avg :'',
+	    						walk3avg :'',
+	    						walk4avg :'',
+	    						walk5avg :'',
+	    						walk1point : 0,
+	    						walk2point : 0,
+	    						walk3point : 0,
+	    						walk4point : 0,
+	    						walk5point : 0,
+	    						tradi1 : '',//대중교통 동일
+	    						tradi2 : '',
+	    						tradi3 : '',
+	    						tradi4 : '',
+	    						tradi5 : '',
+	    						tradi1avg :'',
+	    						tradi2avg :'',
+	    						tradi3avg :'',
+	    						tradi4avg :'',
+	    						tradi5avg :'',
+	    						tradi1point : 0,
+	    						tradi2point : 0,
+	    						tradi3point : 0,
+	    						tradi4point : 0,
+	    						tradi5point : 0,
+	    						totalpoint : ''
+	    				};
+    				}
+    				// 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
+    				var iwContent = '<div style="padding:5px; color:black;">'+item+'</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    				// 인포윈도우를 생성합니다
+    				var infowindow = new daum.maps.InfoWindow({
+    				    content : iwContent
+    				});
+    				// 마커에 마우스오버 이벤트를 등록합니다
+    				daum.maps.event.addListener(marker, 'mouseover', function() {
+    				  // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+    					infowindow.open(map, marker);
+    				});
+    				// 마커에 마우스아웃 이벤트를 등록합니다
+    				daum.maps.event.addListener(marker, 'mouseout', function() {
+    					// 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+    				    infowindow.close();
+    				});
+				});
+			});
+			//alert(coordsList.sample.kaptName);
+			
+		},
+		error : function(e) {
+			console.log(e)
+		}
+	});
+	    	
+}
