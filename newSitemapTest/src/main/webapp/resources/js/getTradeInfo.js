056c085a12992bc4ee52d18c86689518d5d6a220
antirave <antirave1@naver.com>
2017-04-15 14:04:22 +0900

    윤영석
    ㅇㅇㅇ

--- newSitemapTest/src/main/webapp/resources/js/getTradeInfo.js
+++ newSitemapTest/src/main/webapp/resources/js/getTradeInfo.js
@@ -1,28 +1,31 @@
 /**
  * 
  */
-function getTradeInfo(){
+function getTradeInfo(x){
 	//kaptAddr으로 받아서
-	var kaptAddr = '서울특별시 강남구  역삼동 716 역삼동부센트레빌';
+	//var kaptAddr = '서울특별시 강남구  역삼동 716 역삼동부센트레빌';
+	console.log(123);
+	var xloc = null;
+	var yloc = null;
+	var info = null;
 	$.each(perfect,function(index,item){
-		if(item.kaptAddr==kaptAddr){
-			var xloc = item.x;
-			var yloc = item.y;
+		if(item.x==x){
+			xloc = item.y;
+			yloc = item.x;
+			info = item;
 		}
 	})
-	
+	alert(xloc+","+yloc);
 	jQuery.ajaxSettings.traditional = true;
 	 $.ajax({
   	   type:"post",
-  	   async : false,
-  	   contentType : "application/json; charset=utf-8",
   	   url:"getTradeInfo",
   	   data : {
   		   xloc : xloc,
   		   yloc : yloc
   	   },
   	   success : function(data){
-  		   console.log(data)
+  		   console.log(data),console.log(info)
   	   },
   	   error : function(e){
   		   console.log(e)
