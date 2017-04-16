0e1fb43f15ad50acf777c6cc94b8cb7d89adaf48
Crystaldrum <crystalthoon@gmail.com>
2017-04-16 19:44:12 +0900

    태훈 현재 상황, 앞단에서 gwanhal 받아 DB에서 해당 동 모두 뽑아 아파트 리스트 만들기 수정작업 필요
    by LTH

--- newSitemapTest/src/main/java/com/newsite/maptest01/DAO/mapMapper.java
+++ newSitemapTest/src/main/java/com/newsite/maptest01/DAO/mapMapper.java
@@ -12,6 +12,7 @@ public interface mapMapper {
 	public ArrayList<String> getDoroCD(bjdName name);
 	public void kaptcode(kaptCode kaptcode);
 	public ArrayList<String> emdList();
+	public ArrayList<String> getEmdList(String gwanhal);
 	public ArrayList<kaptCode> loadKaptCode(String emd);
 	public ArrayList<aptcode> aptcode();
 	public ArrayList<aptsale> getTradeInfo(String xloc, String yloc);
