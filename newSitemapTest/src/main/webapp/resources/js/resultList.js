5ab1fd73b9aa7b6c88e3e52992d492a5f8bf72a5
yooyoung jeon <user@user-PC>
2017-04-17 16:50:16 +0900

    구, 동 함께 넘겨주기 추가

--- newSitemapTest/src/main/webapp/resources/js/resultList.js
+++ newSitemapTest/src/main/webapp/resources/js/resultList.js
@@ -25,13 +25,13 @@ function buildList(list) {
 	html += '<ol class = "decimal" data-width="400" id = "resultList">';
 
 	
-	moveMap(items[0].citycode);
+	/*moveMap(items[0].citycode);*/
 	createSelectedChart(items[0]);
 	
 	for (var i = 0; i < items.length; i++) {
 		html += '<li><a href = "#" id = "'+items[i].citycode+'" value = "' 
 		+items[i].citycode+'" class = "'+items[i].gu +','+ items[i].dong+'" onclick = "willThisWork('
-		+items[i].citycode+'); moveMap('+items[i].citycode+');"> ' 
+		+items[i].citycode+'); "> ' 
 		+ items[i].si+ " "+ items[i].gu +" "+ items[i].dong + '<a href="#" onclick="removeItem(' + i + ');">   x   </a> '+ '</li>' ;
 	}
 	html += '</ol>';
@@ -77,7 +77,7 @@ function removeItem(index) {
 	
 }
 
-
+/*
 function moveMap(dong){
     	$.ajax({
     		url : "http://apis.vworld.kr/2ddata/ademd/data?apiKey=CCA36BB7-0DA8-3EE7-8836-D4814D529510&domain=http://localhost:8888&emdCd="+dong+"&srsName=EPSG:4326&output=json",
@@ -114,29 +114,61 @@ function moveMap(dong){
     		}
     	});
 }
-
+*/
 function sendData1(){ //코드 및 동 리스트 가져오기
 	var firstlist = [];
-	var codelist = []; //코드리스트
-	var secondlist = [];
+	var resultList = [];
 	
-	for (var j = 0; j < 10; j++) {
-		secondlist[j] = $(".decimal").find("a").eq(j).attr('class');
+	for (var i = 0; i < 10; i++) {
+		firstlist[i] = $(".decimal").find("a").eq(i).attr('class');
 	}
+	console.log(firstlist);//제대로 나옴
 	
-	if (typeof secondlist[6] == undefined ||secondlist[6] == null ) {
-		var dong = secondlist[0] + ',' + secondlist[2]+ ',' +secondlist[4]
-	}else if (typeof secondlist[8] == undefined ||secondlist[8] == null ) {
-		var dong = secondlist[0] + ',' + secondlist[2]+ ',' +secondlist[4]+ ',' +secondlist[6]
-	}
-	else{
-		var dong = secondlist[0] + ',' + secondlist[2]+ ',' +secondlist[4]+ ',' +secondlist[6]+ ',' +secondlist[8];
-	}
-	console.log("code" + codelist);
-	console.log("dong" + dong);
-	
-	var result = [codelist, dong];
-	return result;
+		if (typeof firstlist[6] == undefined ||firstlist[6] == null ) {
+			var split1 = firstlist[0].split(',');
+			var split2 = firstlist[2].split(',');
+			var split3 = firstlist[4].split(',');
+			var obj1 = {gu : split1[0], dong : split1[1] };
+			var obj2 = {gu : split2[0], dong : split2[1] };
+			var obj3 = {gu : split3[0], dong : split3[1] };
+			resultList.push(obj1);
+			resultList.push(obj2);
+			resultList.push(obj3);
+			console.log(split1);
+		}else if (typeof firstlist[8] == undefined ||firstlist[8] == null ) {
+			var split1 = firstlist[0].split(',');
+			var split2 = firstlist[2].split(',');
+			var split3 = firstlist[4].split(',');
+			var split4 = firstlist[6].split(',');
+			var obj1 = {gu : split1[0], dong : split1[1] };
+			var obj2 = {gu : split2[0], dong : split2[1] };
+			var obj3 = {gu : split3[0], dong : split3[1] };
+			var obj4 = {gu : split4[0], dong : split4[1] };
+			resultList.push(obj1);
+			resultList.push(obj2);
+			resultList.push(obj3);
+			resultList.push(obj4);
+		}
+		else{
+			var split1 = firstlist[0].split(',');
+			var split2 = firstlist[2].split(',');
+			var split3 = firstlist[4].split(',');
+			var split4 = firstlist[6].split(',');
+			var split5 = firstlist[8].split(',');
+			var obj1 = {gu : split1[0], dong : split1[1] };
+			var obj2 = {gu : split2[0], dong : split2[1] };
+			var obj3 = {gu : split3[0], dong : split3[1] };
+			var obj4 = {gu : split4[0], dong : split4[1] };
+			var obj5 = {gu : split5[0], dong : split5[1] };
+			resultList.push(obj1);
+			resultList.push(obj2);
+			resultList.push(obj3);
+			resultList.push(obj4);
+			resultList.push(obj5);
+		}
+		
+		console.log(resultList);
+/*	return resultList;*/
 	
 }
 
